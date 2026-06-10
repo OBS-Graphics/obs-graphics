e3e7c2d5dede065b8972ab00aaa114f634df8fec
Diego Lopes <diego95lopes@live.com>
2026-06-10 17:03:00 -0400

    Reorganize into multi-repo structure
    - Remove src/engine (now obs-graphics-engine submodule at engine/)
    - Remove src/editor_v2 (now standalone obs-graphics-editor repo)
    - Add engine as git submodule
    - Update CMakeLists to reference engine/ submodule
    - Allow engine/ in .gitignore allowlist

--- src/editor_v2/icons.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "QtAwesome.h"
-
-fa::QtAwesome* qta();
--- src/editor_v2/ui/widgets/ColorUtils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <QColor>
-#include <QGradient>
-
-QColor interpolateColor(const QColor& a, const QColor& b, qreal t);
-QColor sampleGradient(const QGradientStops& stops, qreal t);
